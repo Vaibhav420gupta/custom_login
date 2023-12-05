13a4b370fbd62e134fdacd8385a4a473d8cdc71a
Vaibhav420gupta <vaibhavcs8853589883@gmail.com>
2023-12-06 00:10:08 +0530

    Second Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    {{-- <h1>Home : {{Auth::user()->name }}</h1> --}}
-
-    <h1 class="text-center">Welcome! to the Laravel Project</h1>
+    <h1 class="text-center">Welcome to the Laravel Project</h1>
 @endsection
